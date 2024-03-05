90bc44060496b73f678307b9e90a438ca346eb4e
Felipe Santos <159580121+felipesantos71@users.noreply.github.com>
2024-03-05 11:22:23 -0300

    Update soma com funcao int e media.c

--- soma com funcao int e media.c
+++ soma com funcao int e media.c
@@ -7,10 +7,10 @@ void cabecalho () {
 	printf("\n== Soma de numeros com Função int ==\n");
 }
 
-int somar(float n1, float n2) {
-	float soma;
-	soma = n1 + n2;
-	return soma;
+float calcularMedia(float n1, float n2) {
+	float media;
+	media = (n1 + n2) / 2;
+	return media;
 }
 
 
@@ -18,7 +18,7 @@ int main () {
 	setlocale(LC_ALL, "");
 	
 	//declarando variaveis
-	float primeiroNumero, segundoNumero, soma, media;
+	float primeiroNumero, segundoNumero, media;
 
 	//solicitando dados do usuario
 	cabecalho();
@@ -30,9 +30,8 @@ int main () {
 	
 	//calulando dados 
 	
-	soma = somar(primeiroNumero, segundoNumero);
+	media = calcularMedia(primeiroNumero, segundoNumero);
 	
-	media = soma / 2;
 	//aprensentando dados ao usuario
 	printf("Media: %.2f", media);
 	
