b323fdd55323c02877beb0f3c8443093af1c0e48
Felipe Santos <159580121+felipesantos71@users.noreply.github.com>
2024-03-12 11:29:22 -0300

    Add files via upload

---
+++ media de aluno com for e do while e funcoes.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+	//inicio do programa
+void cabecalho () {
+	system("cls || clear");
+	printf("\n=== Media de aluno com 3 Notas ===\n");
+}
+char* verificarAprovacao(float media){
+	char resposta[100];
+	
+	if (media >= 7) 
+		strcpy(resposta, "Aprovado!");
+		else if	(media >=5)
+		strcpy(resposta, "Recuperação!");
+		else
+		strcpy(resposta, "Reprovado!");
+
+	return resposta;	
+}
+int main() {
+	setlocale(LC_ALL, "");
+	//declarando variaveis
+	float nota, media, soma;
+	int i;
+	char resultado[100];
+	
+	//calculando nota e por meio do laço
+	cabecalho();
+	for (i=1; i<=3;i++){
+		do {
+			
+			printf("Digite um número de 0 até 10: \n");	
+			printf("Digite o %iº número: ", i);
+			scanf("%f",&nota);
+					
+		} while (nota >10 || nota <0);
+		
+		soma = soma + nota;
+	}
+			
+	//exibindo quantidade de vezes que o laço de repetição aconteceu.
+	media = soma/3;
+	cabecalho();
+	printf("Média aritmética: %.2f\n", media);
+	strcpy(resultado, verificarAprovacao(media));
+	printf("Aluno: %s\n", resultado);
+	
+	return 0;
+}
