37abf8ca4649ca45a5d48d06bf7e958fc238b50e
Felipe Santos <159580121+felipesantos71@users.noreply.github.com>
2024-03-13 08:22:49 -0300

    Add files via upload

---
+++ nota N e media com while.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+//inicio do algoritmo
+void cabecalho() {
+	system("cls || clear");
+	printf("\n == Media de aluno ==\n");
+}
+int main() {
+	setlocale(LC_ALL, "");
+	
+	//declarando variáveis
+	int contador;
+	float soma, nota, media;
+	char continua;
+	
+	//solicitando dados do usuário
+	cabecalho();
+	do {
+		
+		printf("Digite uma nota : ");
+		scanf("%f",&nota);
+		fflush(stdin);
+		contador = contador + 1;
+		
+		printf("Digite 'n' Se deseja escrever mais uma nota : ");
+		scanf("%c",&continua);
+		
+		soma = soma + nota;	
+			
+	} while (continua == 'n');
+	
+	//apresentando dados ao usuário
+	media = soma / contador;
+	cabecalho();
+	printf("Média do aluno: %.2f ", media);
+	
+	return 0;
+//fim do algoritmo
+}
